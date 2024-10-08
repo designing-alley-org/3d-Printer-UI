5a68a0d2d656c8d61d8e6451e14c18b8d7be3d29
Hrp16 <harshraj1604@gmail.com>
2024-10-08 12:29:59 +0530

    storybook issue fixed

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,8 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+// import type { StorybookConfig } from '@storybook/react-vite';
+// import { Configuration as WebpackConfig } from 'webpack';
 
-const config: StorybookConfig = {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const config: any = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 
   addons: [
@@ -20,6 +22,41 @@ const config: StorybookConfig = {
 
   typescript: {
     reactDocgen: 'react-docgen-typescript',
+  },
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(js|jsx|ts|tsx)$/, // Add JSX and TSX file types
+      exclude: /node_modules/, // Exclude node_modules
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [
+              '@babel/preset-env', // Support for modern JS features
+              '@babel/preset-react', // Support for React
+              '@babel/preset-typescript', // If you're using TypeScript
+            ],
+          },
+        },
+      ],
+    });
+
+    config.resolve.extensions.push('.js', '.jsx', '.ts', '.tsx'); // Ensure Webpack resolves these extensions
+
+    return config;
+  },
+  babel: async (options) => {
+    // Add @babel/preset-react to ensure JSX works without React import for React 17+
+    options.presets.push([
+      '@babel/preset-react',
+      {
+        runtime: 'automatic', // This enables the automatic JSX runtime (React 17+)
+      },
+    ]);
+    return options;
   },
 };
 export default config;
