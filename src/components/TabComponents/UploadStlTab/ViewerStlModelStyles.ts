bcd3a1b78e752708080add7e1054b11aaadc38b1
abhimanyujangid <abhimanyujangid79@gmail.com>
2024-10-29 18:47:58 +0530

    change css for mobile screen in viewstlmodel

--- src/components/TabComponents/UploadStlTab/ViewerStlModelStyles.ts
+++ src/components/TabComponents/UploadStlTab/ViewerStlModelStyles.ts
@@ -4,7 +4,7 @@ export const modalContainer = {
   position: 'fixed',
   top: 0,
   left: 0,
-  width: '100vw',
+  width: '100%',
   height: '100vh',
   backgroundColor: 'rgba(0, 0, 0, 0.5)',
   display: 'flex',
