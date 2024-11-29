de5f14a40572dde8e988fe33e50cfe9396c2f4b3
Aadish25 <jainaadish2503@gmail.com>
2024-11-29 12:27:26 +0530

    curve added & notification removed

--- src/pages/YourAccount/AccountLayout.tsx
+++ src/pages/YourAccount/AccountLayout.tsx
@@ -9,6 +9,7 @@ import Settings from './Settings';
 // import api from '../../axiosConfig';
 import { useSelector } from 'react-redux';
 import { getUserOrder } from '../../store/actions/getUserOrder';
+import { Box } from '@mui/material';
 
 const AccountLayout = () => {
   const [activeTab, setActiveTab] = useState<number>(1);
@@ -19,7 +20,7 @@ const AccountLayout = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-      await getUserOrder(setOrders);
+        await getUserOrder(setOrders);
       } catch (error) {
         console.log(error); // Handle errors
       } finally {
@@ -35,20 +36,38 @@ const AccountLayout = () => {
     <AccWrapper>
       <SideTab>
         {accTab.map((item) => (
-          <span
-            key={item.id}
-            className={`${activeTab === item.id ? 'selected' : ''}`}
-            onClick={() => setActiveTab(item.id)}
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
           >
-            {item.label}
-          </span>
+            <span
+              key={item.id}
+              className={`${activeTab === item.id ? 'selected' : ''}`}
+              onClick={() => setActiveTab(item.id)}
+            >
+              {item.label}
+            </span>
+            <Box
+              sx={{
+                height: '5rem',
+                borderLeft: `30px solid ${item.id === activeTab ? 'white' : 'transparent'}`,
+                backgroundColor:
+                  item.id === activeTab ? 'white' : 'transparent',
+                position: 'absolute',
+                left: '2.5%',
+                zIndex: 9,
+                borderRadius: '3rem 0rem 0rem 3rem',
+              }}
+            ></Box>
+          </Box>
         ))}
       </SideTab>
       <MainComp>
         {activeTab === 1 && <MyProfile profileData={user?.user} />}
         {activeTab === 2 && <Orders orderData={orders?.data?.order} />}
-        {activeTab === 3 && <Notification />}
-        {activeTab === 4 && <Settings />}
+        {activeTab === 3 && <Settings />}
       </MainComp>
     </AccWrapper>
   );
