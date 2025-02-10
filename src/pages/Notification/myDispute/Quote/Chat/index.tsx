67a6cbfa26386bb9141333c9ab3ce3b0d642db42
Aadish25 <jainaadish2503@gmail.com>
2025-02-10 19:22:11 +0530

    dispute chat

--- src/pages/Notification/myDispute/Quote/Chat/index.tsx
+++ src/pages/Notification/myDispute/Quote/Chat/index.tsx
@@ -1,37 +1,42 @@
 import { Box } from '@mui/material';
+import { useEffect, useState, useCallback } from 'react';
+import { io, Socket } from 'socket.io-client';
+import { useSelector } from 'react-redux';
+import { DefaultEventsMap } from '@socket.io/component-emitter';
 import ChatFooter from './Footer';
 import ChatBody from './Body';
-import { useEffect, useState } from 'react';
-import { io, Socket } from 'socket.io-client';
-import { DefaultEventsMap } from '@socket.io/component-emitter';
-import api from '../../../axiosConfig';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import api from '../../../../../axiosConfig';
+import { RootState } from '../../../../../store/types';
 
 interface Message {
   sender: string;
   content: string;
   sendBy: string;
   files: any[];
-  id?: string; // Add an id field to track unique messages
+  id?: string;
 }
 
-const  API_URL =  import.meta.env.VITE_API_URL as string;
-export default function Chat() {
+interface IChatProps {
+  disputeId: string;
+}
+
+export default function Chat({ disputeId }: IChatProps) {
+  console.log(disputeId);
+  const user = useSelector((state: RootState) => state.user);
   const [socket, setSocket] = useState<Socket<
     DefaultEventsMap,
     DefaultEventsMap
   > | null>(null);
   const [messages, setMessages] = useState<Message[]>([]);
-  const user = useSelector((state: any) => state.user);
-  const { orderId } = useParams();
+  const defaultUserId = user?.user?._id;
+  const defaultMerchantId = user?.user?._id;
 
-  const defaultUserId = user.user._id;
-  const defaultMerchantId = user.user._id;
-
+  console.log("defaultUserId in dispute", defaultUserId);
+  console.log("defaultMerchantId in dispute", defaultMerchantId);
+  
 
   useEffect(() => {
-    const newSocket: Socket = io( API_URL, {
+    const newSocket: Socket = io(import.meta.env.VITE_API_URL as string, {
       withCredentials: true,
       transports: ['websocket', 'polling'],
     });
@@ -40,12 +45,11 @@ export default function Chat() {
 
     newSocket.on('connect', () => {
       console.log('Connected to the server:', newSocket.id);
-      newSocket.emit('joinChat', defaultUserId, orderId);
-      newSocket.emit('joinChat', defaultMerchantId, orderId);
+      newSocket.emit('joinDispute', defaultUserId, disputeId);
     });
 
     // Only update messages when receiving messages from others
-    newSocket.on('receiveMessage', (newMessage: Message) => {
+    newSocket.on('receiveDisputeMessage', (newMessage: Message) => {      
       // Check if the message is from another user
       if (newMessage.sender !== defaultUserId) {
         setMessages((prevMessages) => [...prevMessages, newMessage]);
@@ -63,14 +67,10 @@ export default function Chat() {
     // Fetch existing messages
     async function fetchMessages() {
       try {
-        const response = await api.get(`/get-message/${orderId}`);
-        if (response.data.status === 'error') {
-          throw new Error(response.data.message);
-        }
+        const response = await api.get(`/get-message/${disputeId}`);
         const fetchedMessages = response.data.data.messages;
-
         const fetchMessages = fetchedMessages.reverse();
-        setMessages(fetchMessages[0].messages);
+        if(fetchMessages.length > 0)   setMessages(fetchMessages[0].messages);
       } catch (error) {
         console.error('Error fetching messages:', error);
       }
@@ -80,7 +80,7 @@ export default function Chat() {
     return () => {
       newSocket.disconnect();
     };
-  }, [orderId, defaultUserId, defaultMerchantId]);
+  }, [disputeId, defaultUserId, defaultMerchantId]);
 
   // Function to handle sending messages
   const handleSendMessage = (content: string, files: any[] = []) => {
@@ -90,7 +90,7 @@ export default function Chat() {
         receiverId: defaultMerchantId,
         files,
         content,
-        order_id: orderId,
+        dispute_id: disputeId,
         sendBy: 'user',
       };
 
@@ -99,13 +99,13 @@ export default function Chat() {
         sender: defaultUserId,
         content,
         sendBy: 'user',
-        files,
+        files: files,
         // id: Date.now().toString(), // Add a temporary unique ID
       };
       setMessages((prevMessages) => [...prevMessages, newMessage]);
 
       // Emit message to server
-      socket.emit('sendMessage', messageData);
+      socket.emit('sendDisputeMessage', messageData);
     }
   };
 
@@ -116,7 +116,7 @@ export default function Chat() {
         socket={socket}
         sender={defaultUserId}
         receiver={defaultMerchantId}
-        orderId={orderId}
+        disputeId={disputeId}
         onSendMessage={handleSendMessage}
       />
     </Box>
