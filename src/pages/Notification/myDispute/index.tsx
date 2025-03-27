5adf296d65663a1a28566e61f6b0a72bbaf01075
abhimanyujangid <abhimanyujangid79@gmail.com>
2025-03-27 16:33:13 +0530

    feat: improve ongoing orders pagination and enhance dispute navigation

--- src/pages/Notification/myDispute/index.tsx
+++ src/pages/Notification/myDispute/index.tsx
@@ -10,6 +10,7 @@ import api from '../../../axiosConfig';
 import { deleteNotification } from '../../../store/notification/notification';
 import { Typography } from '@mui/material';
 import { NotificationCard } from '../../Notification/NotificationCard';
+import { useNavigate } from 'react-router-dom';
 
 interface Dispute {
   _id: string;
@@ -41,6 +42,7 @@ const MyDisputes: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [selectedDispute, setSelectedDispute] = useState<Dispute | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   // Get notifications from Redux store
   const notifications = useSelector((state: any) => state.notification.notification);
@@ -100,6 +102,7 @@ const MyDisputes: React.FC = () => {
     str.charAt(0).toUpperCase() + str.slice(1);
 
   const handleChatOpen = (dispute: Dispute) => async () => {
+    navigate(`/account/my-dispute/${dispute._id}/${dispute.orderId}/${dispute.dispute_type}`);  // This is the URL that the user will be redirected to account dispute page and then to the chat page
     setSelectedDispute(dispute);
     setIsChatOpen(true);
 
