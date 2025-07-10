2100470c0e6b360b52d3b3ed9d98e9ccce1d707e
Abhimanyu jangid <87138461+abhimanyujangid@users.noreply.github.com>
2025-07-10 17:50:40 +0530

    refactor: remove inactive printer filtering from getPrintersByTechnologyAndMaterialService

--- src/services/printer.ts
+++ src/services/printer.ts
@@ -8,8 +8,6 @@ export const getPrintersByTechnologyAndMaterialService = async (
         let response = await api.get(
             `/filter?technology=${selectedTech}&materials=${selectedMat}`
         );
-        // Here 0 = inactive and 1 = active
-        response.data.data = response.data.data.filter((printer: any) => printer.status === 1);
         return response.data.data || []; 
     } catch (error) {
         console.error("Error fetching printer data:", error);
