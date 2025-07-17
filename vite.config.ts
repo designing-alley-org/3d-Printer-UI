9d6f9ff852938f834a474667ec807c5cf4ac0063
Aadish25 <jainaadish2503@gmail.com>
2025-07-17 22:27:36 +0530

    build issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['jwt-decode'],
+    include: ['jwt-decode', 'lodash-es'],
   },
   resolve: {
     extensions: ['.ts', '.tsx'],
+    alias: {
+      'lodash-es': path.resolve(__dirname, 'node_modules/lodash-es'),
+    },
   },
+  build: {
+    rollupOptions: {
+      external: [],
+    }
+  }
 });
