3f9ad0944bd65075bb3046ec48327e203eb7b560
Abhimanyu jangid <87138461+abhimanyujangid@users.noreply.github.com>
2025-09-16 07:01:28 +0530

    feat: add error handling to email verification services; import returnError utility for improved error messaging

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,7 +1,7 @@
 import toast from "react-hot-toast";
 import api from "../axiosConfig";
 import { editUser } from "../types";
-import { returnResponse } from "../utils/function";
+import { returnError, returnResponse } from "../utils/function";
 
 
 export const updateUserService = async (userData: editUser): Promise<any> => {
@@ -47,6 +47,26 @@ export const sendPasswordResetService = async (email: string): Promise<any> => {
 export const resetPasswordService = async (newPassword: string, token: string): Promise<any> => {
     try {
       const response = await api.post(`/reset-password`, { newPassword, token });
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+
+export const sendUpdateEmailVerificationService = async (new_email: string): Promise<any> => {
+    try {
+      const response = await api.post(`/send-update-email-verification`, { new_email });
+      return returnResponse(response);
+    } catch (error) {
+      toast.error(returnError(error));
+      throw error;
+    }
+  }
+
+export const updateVerifyUserEmailService = async (new_email: string, token: string): Promise<any> => {
+    try {
+      const response = await api.post(`/update-verify-user-email`, { new_email, token });
       return response;
     } catch (error) {
       throw error;
